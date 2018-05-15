ddfcd5d85731e3ceb18c8f63a0f270564b820df6
amrilsyaifa <38855488+amrilsyaifa@users.noreply.github.com>
2018-05-15 14:31:46 +0700

    Update UserTableSeeder.php

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,6 +12,12 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role_administrator = new User();
+        $role_administrator->role_id = '1';
+        $role_administrator->name = 'administrator';
+        $role_administrator->username = 'Administrator';
+        $role_administrator->email = 'administrator@laravel.com';
+        $role_administrator->password = bcrypt('administrator');
+        $role_administrator->save();
     }
 }
